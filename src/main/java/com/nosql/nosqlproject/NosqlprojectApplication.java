03eb3734ba087964d12fb45ad00120ae5ad2c70d
pikapikapi <pikapikapi_kaori@icloud.com>
2021-10-31 13:24:45 +0800

    Add element-ui support

--- src/main/java/com/nosql/nosqlproject/NosqlprojectApplication.java
+++ src/main/java/com/nosql/nosqlproject/NosqlprojectApplication.java
@@ -1,7 +1,13 @@
 package com.nosql.nosqlproject;
 
+import lombok.Value;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NosqlprojectApplication {
