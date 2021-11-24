24d1b18ceba5a182cfe97125a62c5d4cb7bb94f8
pikapikapikaori <89848201+pikapikapikaori@users.noreply.github.com>
2021-11-24 11:09:09 +0800

    Update structure

---
+++ src/main/java/com/nosql/nosqlproject/repository/Demand16.java
@@ -0,0 +1,15 @@
+package com.nosql.nosqlproject.repository;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Property;
+
+public class Demand16 {
+    private Long id;
+
+    private String name;
+
+    private String direction;
+
+    private int count;
+}
---
+++ src/main/java/com/nosql/nosqlproject/repository/Demand4.java
@@ -0,0 +1,17 @@
+package com.nosql.nosqlproject.repository;
+
+import com.nosql.nosqlproject.entity.Station;
+
+import java.util.List;
+
+public class Demand4 {
+    private Long id;
+
+    private int runtime;
+
+    private String lineName;
+
+    private boolean is_up;
+
+    private List<Station> stations;
+}
