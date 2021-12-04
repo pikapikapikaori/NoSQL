a4ecb3aea3312b09a79e6c3e4594c2363c218f3d
defenqi11 <diaozehao@163.com>
2021-12-04 22:25:02 +0800

    update sort methods

--- src/main/java/com/nosql/nosqlproject/service/SortByCount.java
+++ src/main/java/com/nosql/nosqlproject/service/SortByCount.java
@@ -8,6 +8,8 @@ public class SortByCount implements Comparator {
     public int compare(Object o1,Object o2){
         if(((Demand15)o1).count < ((Demand15)o2).count)
             return 1;
-        return -1;
+        if(((Demand15)o1).count > ((Demand15)o2).count)
+            return -1;
+        return 0;
     }
 }
--- src/main/java/com/nosql/nosqlproject/service/SortByNum.java
+++ src/main/java/com/nosql/nosqlproject/service/SortByNum.java
@@ -6,6 +6,8 @@ class SortByNum implements Comparator {
     public int compare(Object o1,Object o2){
         if(((StationLines)o1).num < ((StationLines)o2).num)
             return 1;
-        return -1;
+        if(((StationLines)o1).num > ((StationLines)o2).num)
+            return -1;
+        return 0;
     }
 }
