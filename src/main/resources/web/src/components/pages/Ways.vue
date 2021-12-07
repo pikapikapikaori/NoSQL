cea170f26ad06c2327e9b5f935322834e1b9242e
pikapikapikaori <89848201+pikapikapikaori@users.noreply.github.com>
2021-12-07 23:58:28 +0800

    Add UI: demand 5, 11a

--- src/main/resources/web/src/components/pages/Ways.vue
+++ src/main/resources/web/src/components/pages/Ways.vue
@@ -10,35 +10,101 @@
         </el-header>
 
         <el-main style="text-align: left; font-size: 18px">
-          <div class="spacehd" style="text-align: center">
-            {{'\xa0'}}
-          </div>
+          <el-tabs type="border-card">
+            <el-tab-pane label="直达路线查询">
+              直达路线查询
+              <el-divider></el-divider>
 
-          <div class="forminline" style="text-align: center">
-            <el-form :inline="true" @submit.native.prevent :model="formInline" class="demo-form-inline">
-              <el-form-item label="站点1">
-                <el-input v-model="formInline.station1" placeholder="站点1"></el-input>
-              </el-form-item>
+              <div class="forminline" style="text-align: center">
+                <el-form :inline="true" @submit.native.prevent :model="formInline" class="demo-form-inline">
+                  <el-form-item label="站点1">
+                    <el-input v-model="formInline.station1" placeholder="站点1"></el-input>
+                  </el-form-item>
 
-              <el-form-item label="站点2">
-                <el-input v-model="formInline.station2" placeholder="站点2"></el-input>
-              </el-form-item>
+                  <el-form-item label="站点2">
+                    <el-input v-model="formInline.station2" placeholder="站点2"></el-input>
+                  </el-form-item>
 
-              <el-form-item>
-                <el-button type="primary" @click="searchRoutes" native-type="submit">查询</el-button>
-                <el-button type="primary" @click="clearAll" native-type="submit">清空</el-button>
-              </el-form-item>
-            </el-form>
+                  <el-form-item>
+                    <el-button type="primary" @click="searchRoutes" native-type="submit">查询</el-button>
+                    <el-button type="primary" @click="clearAll" native-type="submit">清空</el-button>
+                  </el-form-item>
+                </el-form>
 
-            <div>
-              <div v-if="isClear">
-                <el-table :data="searchWayResult" stripe="true" :header-cell-style="{textAlign: 'center'}" :cell-style="{textAlign: 'center'}" empty-text="暂无直达路线">
-                  <el-table-column prop="route" label="路线" width="980">
-                  </el-table-column>
-                </el-table>
+                <div>
+                  <div v-if="isClear">
+                    <el-table :data="searchWayResult" stripe="true" :header-cell-style="{textAlign: 'center'}" :cell-style="{textAlign: 'center'}" empty-text="暂无直达路线">
+                      <el-table-column prop="route" label="路线" width="980">
+                      </el-table-column>
+                    </el-table>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </el-tab-pane>
+            <!--
+
+            <el-tab-pane label="最短路线查询（基于Id）">
+              最短路线查询（基于Id）
+              <el-divider></el-divider>
+
+              <div class="idShorrtedRouteForm" style="text-align: center">
+                <el-form :inline="true" @submit.native.prevent :model="idShortedRouteForm" class="demo-form-inline">
+                  <el-form-item label="站点1 Id">
+                    <el-input v-model="idShortedRouteForm.station1" placeholder="站点1 Id"></el-input>
+                  </el-form-item>
+
+                  <el-form-item label="站点2 Id">
+                    <el-input v-model="idShortedRouteForm.station2" placeholder="站点2 Id"></el-input>
+                  </el-form-item>
+
+                  <el-form-item>
+                    <el-button type="primary" @click="searchRoutesId" native-type="submit">查询</el-button>
+                    <el-button type="primary" @click="clearAllId" native-type="submit">清空</el-button>
+                  </el-form-item>
+                </el-form>
+
+                <div>
+                  <div v-if="isClearId">
+                    <el-table :data="searchIdResult" stripe="true" :header-cell-style="{textAlign: 'center'}" :cell-style="{textAlign: 'center'}" empty-text="暂无直达路线">
+                      <el-table-column prop="route" label="路线" width="980">
+                      </el-table-column>
+                    </el-table>
+                  </div>
+                </div>
+              </div>
+            </el-tab-pane>
+
+            <el-tab-pane label="最短路线查询（基于名称）">
+              最短路线查询（基于名称）
+              <el-divider></el-divider>
+              <div class="nameShorrtedRouteForm" style="text-align: center">
+                <el-form :inline="true" @submit.native.prevent :model="nameShortedRouteForm" class="demo-form-inline">
+                  <el-form-item label="站点1 名称">
+                    <el-input v-model="nameShortedRouteForm.station1" placeholder="站点1 名称"></el-input>
+                  </el-form-item>
+
+                  <el-form-item label="站点2 名称">
+                    <el-input v-model="nameShortedRouteForm.station2" placeholder="站点2 名称"></el-input>
+                  </el-form-item>
+
+                  <el-form-item>
+                    <el-button type="primary" @click="searchRoutesName" native-type="submit">查询</el-button>
+                    <el-button type="primary" @click="clearAllName" native-type="submit">清空</el-button>
+                  </el-form-item>
+                </el-form>
+
+                <div>
+                  <div v-if="isClearName">
+                    <el-table :data="searchNameResult" stripe="true" :header-cell-style="{textAlign: 'center'}" :cell-style="{textAlign: 'center'}" empty-text="暂无直达路线">
+                      <el-table-column prop="route" label="路线" width="980">
+                      </el-table-column>
+                    </el-table>
+                  </div>
+                </div>
+              </div>
+            </el-tab-pane>
+            -->
+          </el-tabs>
         </el-main>
 
         <el-footer>
@@ -57,13 +123,25 @@ export default {
   name: "Ways",
   data(){
     return{
-      headmsg: '直达路线查询',
+      headmsg: '路线查询',
       isClear: false,
       formInline: {
         station1: '',
         station2: ''
       },
-      searchWayResult: []
+      searchWayResult: [],
+      isClearId: false,
+      idShortedRouteForm: {
+        station1: '',
+        station2: ''
+      },
+      searchIdResult: [],
+      isClearName: false,
+      nameShortedRouteForm: {
+        station1: '',
+        station2: ''
+      },
+      searchNameResult: []
     }
   },
   components:{
@@ -89,6 +167,44 @@ export default {
     },
     clearAll(){
       this.isClear = false;
+    },
+    searchRoutesId() {
+      const axios = require('axios');
+
+      this.isClearId = true;
+
+      axios.get('http://localhost:8080/line/find_shortestRoute_id', {
+        params: {
+          station1: this.idShortedRouteForm.station1,
+          station2: this.idShortedRouteForm.station2
+        }
+      }).then((res) => {
+        this.searchIdResult = res.data;
+      }).catch((err) => {
+        this.searchIdResult = [];
+      });
+    },
+    clearAllId(){
+      this.isClearId = false;
+    },
+    searchRoutesName() {
+      const axios = require('axios');
+
+      this.isClearName = true;
+
+      axios.get('http://localhost:8080/line/find_shortestRoute_name', {
+        params: {
+          station1: this.nameShortedRouteForm.station1,
+          station2: this.nameShortedRouteForm.station2
+        }
+      }).then((res) => {
+        this.searchNameResult = res.data;
+      }).catch((err) => {
+        this.searchNameResult = [];
+      });
+    },
+    clearAllName(){
+      this.isClearName = false;
     }
   }
 }
